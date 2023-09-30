2beb09100d83c875123e5f5a88271cfdc5b5980b
Tomiwa Idowu <tomiwaidowuu@gmail.com>
2023-09-30 02:32:34 +0100

    chore: update custom painter

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -10,14 +10,121 @@ class HomePage extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage> {
+class _HomePageState extends ConsumerState<HomePage>
+    with SingleTickerProviderStateMixin {
   final snapshotController = SnapshotController();
   bool isDarkMode = false;
+  double radius = 0.0;
+  late final AnimationController _animationController;
+  late final Animation<double> _radiusAnimation;
+  // late final Animation<double> _animation;
+
+  late Animation<double> fadeTransition;
+  late Animation<double> scaleTransition;
+
+  late CustomSnapshotPainter customSnapshotPainter;
+
+  static final Animatable<double> _fadeInTransition = Tween<double>(
+    begin: 0.0,
+    end: 1.00,
+  ).chain(CurveTween(curve: const Interval(0.125, 0.250)));
+
+  static final Animatable<double> _scaleDownTransition = Tween<double>(
+    begin: 1.10,
+    end: 1.00,
+  ).chain(TweenSequence<double>([
+    TweenSequenceItem<double>(
+      tween: Tween<double>(begin: 0.0, end: 0.4)
+          .chain(CurveTween(curve: const Cubic(0.05, 0.0, 0.133333, 0.06))),
+      weight: 0.166666,
+    ),
+    TweenSequenceItem<double>(
+      tween: Tween<double>(begin: 0.4, end: 1.0)
+          .chain(CurveTween(curve: const Cubic(0.208333, 0.82, 0.25, 1.0))),
+      weight: 1.0 - 0.166666,
+    ),
+  ]));
+
+  static final Animatable<double> _scaleUpTransition = Tween<double>(
+    begin: 0.85,
+    end: 1.00,
+  ).chain(TweenSequence<double>([
+    TweenSequenceItem<double>(
+      tween: Tween<double>(begin: 0.0, end: 0.4)
+          .chain(CurveTween(curve: const Cubic(0.05, 0.0, 0.133333, 0.06))),
+      weight: 0.166666,
+    ),
+    TweenSequenceItem<double>(
+      tween: Tween<double>(begin: 0.4, end: 1.0)
+          .chain(CurveTween(curve: const Cubic(0.208333, 0.82, 0.25, 1.0))),
+      weight: 1.0 - 0.166666,
+    ),
+  ]));
+
+  static final Animatable<double?> _scrimOpacityTween = Tween<double?>(
+    begin: 0.0,
+    end: 0.60,
+  ).chain(CurveTween(curve: const Interval(0.2075, 0.4175)));
+
+  void _updateAnimations() {
+    // _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+    //   CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    // );
+    fadeTransition = 1 == 2
+        ? kAlwaysCompleteAnimation
+        : _fadeInTransition.animate(Tween<double>(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation(
+                parent: _animationController, curve: Curves.easeInOut),
+          ));
+
+    scaleTransition = (1 == 2 ? _scaleDownTransition : _scaleUpTransition)
+        .animate(Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    ));
+
+    // _animation.addListener(() {});
+    // _animation.addStatusListener((status) {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    scaleTransition = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    fadeTransition = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _radiusAnimation = Tween<double>(begin: 0.0, end: 1000.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+    _updateAnimations();
+    customSnapshotPainter = CustomSnapshotPainter(
+      reverse: false,
+      fade: fadeTransition,
+      scale: scaleTransition,
+      radius: _radiusAnimation,
+      radiusColor: isDarkMode ? Colors.black : Colors.white,
+      animation: _animationController,
+    );
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return AdaptiveScaffold(
       snapshotController: snapshotController,
       title: 'Dark Mode Animation',
+      radius: _radiusAnimation.value,
+      circleColor: isDarkMode ? Colors.white : Colors.black,
+      customSnapshotPainter: customSnapshotPainter,
       body: Column(
         children: [
           AdaptiveListSection(
@@ -53,12 +160,27 @@ class _HomePageState extends ConsumerState<HomePage> {
           ),
         ],
       ),
-      onThemeIconPressed: () {
+      onThemeIconPressed: () async {
+        print(_radiusAnimation.value);
         final mode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
-        ref.read(themeProvider.notifier).changeTheme(mode);
+        if (!_animationController.isCompleted) {
+          _animationController.forward();
+        } else {
+          _animationController.reset();
+        }
+        snapshotController.allowSnapshotting = true;
+        setState(() {});
+        await Future.delayed(const Duration(
+            milliseconds:
+                600)); // await Future.delayed(const Duration(milliseconds: 500));
+        if (_animationController.status == AnimationStatus.forward) {
+          _animationController.reset();
+        }
         setState(() {
+          snapshotController.allowSnapshotting = false;
           isDarkMode = !isDarkMode;
         });
+        ref.read(themeProvider.notifier).changeTheme(mode);
       },
     );
   }
--- lib/widgets/adaptive_scaffold.dart
+++ lib/widgets/adaptive_scaffold.dart
@@ -12,13 +12,18 @@ class AdaptiveScaffold extends StatelessWidget {
     required this.body,
     required this.snapshotController,
     this.onThemeIconPressed,
+    this.radius = 0.0,
+    this.circleColor = Colors.white,
+    required this.customSnapshotPainter,
   });
 
   final String? title;
   final Widget body;
   final SnapshotController snapshotController;
   final VoidCallback? onThemeIconPressed;
-
+  final double radius;
+  final Color circleColor;
+  final CustomSnapshotPainter customSnapshotPainter;
   @override
   Widget build(BuildContext context) {
     Widget? widget;
@@ -41,6 +46,7 @@ class AdaptiveScaffold extends StatelessWidget {
         ),
       );
     } else {
+      // PageTransitionsTheme()
       widget = Scaffold(
         appBar: title != null
             ? AppBar(
@@ -56,16 +62,70 @@ class AdaptiveScaffold extends StatelessWidget {
         body: body,
       );
     }
-    return SnapshotWidget(
-      controller: snapshotController,
-      painter: CustomSnapshotPainter(),
-      child: widget,
+    return IgnorePointer(
+      ignoring: false,
+      child: SnapshotWidget(
+        controller: snapshotController,
+        mode: SnapshotMode.permissive,
+        painter: customSnapshotPainter,
+        autoresize: true,
+        child: widget,
+      ),
+      // Stack(
+      //   children: [
+      //     SnapshotWidget(
+      //       controller: snapshotController,
+      //       mode: SnapshotMode.permissive,
+      //       painter: customSnapshotPainter,
+      //       autoresize: true,
+      //       child: widget,
+      //     ),
+      //     // Align(
+      //     //   alignment: Alignment.topRight,
+      //     //   child: Transform.scale(
+      //     //     scale: radius,
+      //     //     child: Container(
+      //     //       height: 70,
+      //     //       width: 70,
+      //     //       decoration: BoxDecoration(
+      //     //         color: circleColor,
+      //     //         shape: BoxShape.circle,
+      //     //       ),
+      //     //     ),
+      //     //   ),
+      //     // ),
+      //   ],
+      // ),
     );
   }
 }
 
 class CustomSnapshotPainter extends SnapshotPainter {
-  CustomSnapshotPainter();
+  CustomSnapshotPainter({
+    required this.reverse,
+    required this.scale,
+    required this.fade,
+    required this.animation,
+    required this.radius,
+    required this.radiusColor,
+  }) {
+    animation.addListener(notifyListeners);
+    animation.addStatusListener(_onStatusChange);
+    scale.addListener(notifyListeners);
+    fade.addListener(notifyListeners);
+    radius.addListener(notifyListeners);
+  }
+
+  void _onStatusChange(_) {
+    notifyListeners();
+  }
+
+  final bool reverse;
+  final Animation<double> animation;
+  final Animation<double> scale;
+  final Animation<double> fade;
+  final Animation<double> radius;
+  final Color radiusColor;
   @override
   void paint(PaintingContext context, Offset offset, Size size,
       PaintingContextCallback painter) {
@@ -78,10 +138,18 @@ class CustomSnapshotPainter extends SnapshotPainter {
   @override
   void paintSnapshot(PaintingContext context, ui.Offset offset, ui.Size size,
       ui.Image image, ui.Size sourceSize, double pixelRatio) {
+    context.canvas.drawCircle(
+      Offset(size.width, 0),
+      radius.value,
+      Paint()..color = Colors.white,
+    );
     final Rect src = Rect.fromLTWH(0, 0, sourceSize.width, sourceSize.height);
     final Rect dst =
         Rect.fromLTWH(offset.dx, offset.dy, size.width, size.height);
-    final Paint paint = Paint()..filterQuality = FilterQuality.low;
+    final Paint paint = Paint()
+      ..filterQuality = FilterQuality.low
+      ..color = const ui.Color.fromARGB(228, 255, 255, 250);
     context.canvas.drawImageRect(image, src, dst, paint);
+    // _drawImageScaledAndCentered(context, image, 1, 0.5, pixelRatio);
   }
 }
